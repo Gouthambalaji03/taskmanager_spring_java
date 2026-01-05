1e307bf469258f13f60f7fc1e96b6071f7538f38
avina03 <avina.chembu@gmail.com>
2026-01-05 23:54:20 +0530

    minor changes

--- src/main/java/com/example/taskmanager_spring_java/TaskmanagerSpringJavaApplication.java
+++ src/main/java/com/example/taskmanager_spring_java/TaskmanagerSpringJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskmanagerSpringJavaApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(TaskmanagerSpringJavaApplication.class, args);
     }
-
 }

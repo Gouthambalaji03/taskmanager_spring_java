dd17a2d49d5a039351c3b40fdf3d8bc76c7f108b
avina03 <avina.chembu@gmail.com>
2026-01-06 00:19:59 +0530

    sql query file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO tasks (id, title, description, deadline, completed) VALUES (1, 'Buy groceries', 'Milk, eggs, bread', '2024-12-01', false);
+INSERT INTO tasks (id, title, description, deadline, completed) VALUES (2, 'Read book', 'Read chapter 4', '2024-12-05', false);
